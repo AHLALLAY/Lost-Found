08d41b20292879f756a489b4cbf454eb3e9fa118
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-21 22:55:16 +0100

    feat: Ajouter les routes vers les view 'posts' et 'addPostForm'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/', function () {return view('posts');});
+
+Route::post('/addPostForm', function() {return view('addPostForm');});
+
