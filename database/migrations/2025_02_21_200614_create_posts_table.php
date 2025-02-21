d05193c8c22e973592b768b9874f77808027bea3
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-21 21:21:29 +0100

    feat: Ajouter les migrations et les exécutes

---
+++ database/migrations/2025_02_21_200614_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up() {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->string('photo')->nullable();
+            $table->string('category');
+            $table->enum('lost_found', ['lost', 'found']);
+            $table->date('date');
+            $table->string('location');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+            $table->enum('status', ['open', 'resolved'])->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
