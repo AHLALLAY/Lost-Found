c2f7cd018e306c29f7f16ad7328494c3e98104d8
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-23 23:57:17 +0100

    fix: Régler les routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {return view('posts');});
+// Page d'accueil
+Route::get('/', [PostController::class, 'index']);
 
-Route::post('/addPostForm', function() {return view('addPostForm');});
+// Afficher le formulaire de création
+Route::get('/addPostForm', [PostController::class, 'showPostForm']);
 
+// Soumettre le formulaire de création
+Route::post('/addPost', [PostController::class, 'addPost']);
